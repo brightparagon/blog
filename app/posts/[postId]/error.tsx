e939088e32719a119cce1f8389df6a9e69b0a5ba
Kyeongmo Noh <kyeongmo2@gmail.com>
2024-01-10 11:13:37 +0900

    add error pages

---
+++ app/posts/[postId]/error.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+import { ErrorPage } from 'pages/ErrorPage'
+
+export default function Error(props: { error: Error & { digest?: string }; reset: () => void }) {
+  return <ErrorPage {...props} />
+}
---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,14 @@
+import Layout from 'components/Layout';
+import { useEffect } from 'react';
+
+export function ErrorPage({ error }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <Layout>
+      <h2>에러가 생겼어요 🥲🥲🥲</h2>
+    </Layout>
+  )
+}
